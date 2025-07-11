c1693c235649598bfeb525e289d82c54d6db9976
Vagner Bruno <vagnerbruno0692gmail.com>
2025-07-10 21:40:30 -0300

    Versão 10/07

--- src/app/componentes/filme/filme.ts
+++ src/app/componentes/filme/filme.ts
@@ -8,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Filme {
 
+  alternarFavorito(){
+    this.filme1.favorito = !this.filme1.favorito
+
+  }
+
+  filme1 = {
+    nome:"À Procura da Felicidade",
+    imagem: "/aProcuradafelecidade.jfif",
+    sinopse: "Um pai solo batalha contra a pobreza para criar o filho.",
+    favorito: false
+
+  }
+
+  // filme2 = {
+  //   nome:"Batman: O Cavaleiro das Trevas",
+  //   imagem: "/batmanOCavaleiroDasTrevas.jfif",
+  //   sinopse: "O Coringa surge para desafiar Batman e mergulhar Gotham no caos."
+  // }
+
+  // filme3 = {
+  //   nome:"De Olhos Bem Fechados",
+  //   imagem: "/deOlhosBemFechados.webp",
+  //   sinopse: "A fantasia de Alice leva o marido Bill a um perigoso culto sexual secreto."
+  // }
+
 }
